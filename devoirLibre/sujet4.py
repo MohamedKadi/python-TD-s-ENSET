0bbcc21a88b10b0dd94d4428cbd52356ee9971ff
yikes <gamesmohamed14@gmail.com>
2025-11-05 17:48:09 +0100

    debut sujet 4

---
+++ devoirLibre/sujet4.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# etape1
+
+vehicules = [
+    {"id": 1, "position": np.array([0, 0]),"disponible":True},
+    {"id": 2, "position": np.array([5, 1]),"disponible": True},
+    {"id":3, "position": np.array([2, 3]),"disponible": True},
+    {"id":4,"position":np.array([8, 8]), "disponible": True},
+]
+
+
+clients_en_attente = [
+    {"id_client": 101, "position": np.array([1, 0])},
+    {"id_client": 102, "position": np.array([6, 2])},
+    {"id_client": 103, "position": np.array([3, 4])},
+    {"id_client": 104, "position": np.array([9, 9])},
+]
+
+# etape2
+def calculer_distance(pos1: np.ndarray, pos2: np.ndarray):
+    diff = pos1 - pos2
+    return float(np.sqrt((diff ** 2).sum()))
+
+# etape3
+assignations = [] 
+
+#for client in clients_en_attente:
+    
