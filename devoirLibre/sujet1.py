0170b4adb84799337e2c3ce7683d34ccc70621b6
yikes <gamesmohamed14@gmail.com>
2025-11-05 18:58:27 +0100

    final

--- devoirLibre/sujet1.py
+++ devoirLibre/sujet1.py
@@ -13,33 +13,32 @@ entrepots = {
 }
 
 client_cmd = {"produit1":2,"peoduit2":3} #etc
-"""
 #etape 2
 from sympy import *
 cout_exp = input("entrer le cout comme cette form (ex: 2 * quantite + 5).")
 
 quantite = symbols('quantite')
-#print(quantite)
 
 exp_symbolique = sympify(cout_exp)
-#print(exp_symbolique)
+print(exp_symbolique)
 
 fonc_exp = lambdify((quantite),exp_symbolique,'numpy')
-result = fonc_exp(10)
-print(result)
-"""
+#result = fonc_exp(10)
+#print(result)
+
+
 #etape 3
 def trouver_solution(commande, entrepots):
     plan_approvisionnement = {}
     cout_total = 0
     sorted_entrepots = sorted(entrepots.items(),key=lambda item: item[1]['cout'])
     for nom_entrepot , infos in sorted_entrepots:
-        print(nom_entrepot + " info "+ str(infos))
+        #print(nom_entrepot + " info "+ str(infos))
         for produit, qte in list(commande.items()):
             if qte <= 0:
                 continue
 
-            dispo = infos['stock'].get(produit, 0) # 0 for default value
+            dispo = infos['stock'].get(produit, 0) # si le produit n'est exist pas
 
             if dispo <= 0:
                 continue
@@ -54,40 +53,9 @@ def trouver_solution(commande, entrepots):
                 plan_approvisionnement[nom_entrepot].get(produit, 0) + pris
             )
 
-            cout_total += pris * infos['cout']
+            cout_total += fonc_exp(pris) * infos['cout']
 
     return plan_approvisionnement, cout_total
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 entrepots = {
     "Entrepot_A": {
@@ -110,12 +78,11 @@ commande = {
     "ProduitZ": 5
 }
 
-# --- Run the function ---
-plan, cout_total = trouver_solution(commande, entrepots)
-
-# --- Display results ---
+#etape4
+plan_app, cout_total = trouver_solution(commande, entrepots)
+print(plan_app)
 print("\n=== PLAN D'APPROVISIONNEMENT ===")
-for entrepot, produits in plan.items():
+for entrepot, produits in plan_app.items():
     for produit, quantite in produits.items():
         print(f"Prendre {quantite} de {produit} chez {entrepot}")
 
--- devoirLibre/sujet4.py
+++ devoirLibre/sujet4.py
@@ -3,10 +3,10 @@ import numpy as np
 # etape1
 
 vehicules = [
-    {"id": 1, "position": np.array([0, 0]),"disponible":True},
-    {"id": 2, "position": np.array([5, 1]),"disponible": True},
-    {"id":3, "position": np.array([2, 3]),"disponible": True},
-    {"id":4,"position":np.array([8, 8]), "disponible": True},
+    {"id": 1, "position": np.array([0, 0]), "disponible": True},
+    {"id": 2, "position": np.array([5, 1]), "disponible": True},
+    {"id": 3, "position": np.array([2, 3]), "disponible": True},
+    {"id": 4, "position": np.array([8, 8]), "disponible": True},
 ]
 
 
@@ -18,12 +18,38 @@ clients_en_attente = [
 ]
 
 # etape2
-def calculer_distance(pos1: np.ndarray, pos2: np.ndarray):
+def calculer_distance(pos1, pos2):
     diff = pos1 - pos2
     return float(np.sqrt((diff ** 2).sum()))
 
 # etape3
 assignations = [] 
 
-#for client in clients_en_attente:
-    
+for client in clients_en_attente:
+    vehicules_dispos = list(filter(lambda v: v["disponible"], vehicules))
+
+    if not vehicules_dispos:
+        print("aucune vehicule disponible")
+        break
+
+    distances = list(map(
+        lambda v: calculer_distance(client["position"], v["position"]),
+        vehicules_dispos
+    ))
+
+    couples = list(zip(distances, vehicules_dispos))
+
+    distance_min, vehicule_elu = min(couples, key=lambda x: x[0])
+
+    print(f"Client {client['id_client']} -> Véhicule {vehicule_elu['id']} (distance {distance_min:.2f})")
+    assignations.append((client["id_client"], vehicule_elu["id"]))
+
+    vehicule_elu["disponible"] = False
+
+# etape4
+print("\nAssignations finales :")
+print(assignations)
+
+print("\nÉtat final des véhicules :")
+for v in vehicules:
+    print(f"Véhicule {v['id']} | Position={v['position']} | Disponible={v['disponible']}")
