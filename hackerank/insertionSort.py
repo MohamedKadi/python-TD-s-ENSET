bf18fd6e5237d684e862325367fe6777c319cb3f
yikes <gamesmohamed14@gmail.com>
2025-12-04 20:47:06 +0100

    insertionSort correction

--- hackerank/insertionSort.py
+++ hackerank/insertionSort.py
@@ -14,13 +14,12 @@ def insertionSort1(n, arr):
     max = n-1
     last = arr[max]
     while max > 0:
-        if(arr[max] < last):
-            arr[max+1] = last
+        if(arr[max-1] < last):
+            arr[max] = last
             print(str(arr))
             break
         arr[max] = arr[max-1]
         print(arr)
-        print(max)
         max -= 1
 
 insertionSort1(5, [2, 4, 6, 8, 3])
